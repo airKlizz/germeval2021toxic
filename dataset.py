4af0dcd3052dbffbd2c41eff62248918d3d476e4
airKlizz <remi.calizzano@gmail.com>
2021-05-21 14:02:08 +0200

    Typer CLI

--- dataset.py
+++ dataset.py
@@ -3,14 +3,9 @@ import random
 from datasets import load_dataset
 from transformers import AutoTokenizer
 
-from parameters import (MODEL_CHECKPOINT, SEED, TRAIN_CSV, TRAIN_RATIO,
-                        TRAIN_TEST_CSV, TRAIN_TRAIN_CSV)
 
-random.seed(SEED)
-
-
-def split(train_ratio=TRAIN_RATIO):
-    with open(TRAIN_CSV) as f:
+def split(train_ratio, train_csv, train_train_csv, train_test_csv):
+    with open(train_csv) as f:
         data = f.read().split("\n")
     header = data[0]
     data = data[1:]
@@ -18,13 +13,13 @@ def split(train_ratio=TRAIN_RATIO):
     train_data = data[: int(len(data) * train_ratio)]
     test_data = data[int(len(data) * train_ratio) :]
     assert len(train_data) + len(test_data) == len(data)
-    with open(TRAIN_TRAIN_CSV, "w") as f:
+    with open(train_train_csv, "w") as f:
         f.write(header)
         f.write("\n")
         for elem in train_data:
             f.write(elem)
             f.write("\n")
-    with open(TRAIN_TEST_CSV, "w") as f:
+    with open(train_test_csv, "w") as f:
         f.write(header)
         f.write("\n")
         for elem in test_data:
@@ -32,25 +27,25 @@ def split(train_ratio=TRAIN_RATIO):
             f.write("\n")
 
 
-def load_train(preprocess=False):
-    data = load_dataset("csv", data_files=[TRAIN_TRAIN_CSV])
+def load_train(train_train_csv, model_checkpoint=None, preprocess=False):
+    data = load_dataset("csv", data_files=[train_train_csv])
     dataset = data["train"]
     if preprocess:
-        return preprocess_dataset(dataset)
+        return preprocess_dataset(dataset, model_checkpoint)
     return dataset
 
 
-def load_test(preprocess=False):
-    data = load_dataset("csv", data_files=[TRAIN_TEST_CSV])
+def load_test(train_test_csv, model_checkpoint=None, preprocess=False):
+    data = load_dataset("csv", data_files=[train_test_csv])
     dataset = data["train"]
     if preprocess:
-        return preprocess_dataset(dataset)
+        return preprocess_dataset(dataset, model_checkpoint)
     return dataset
 
 
-def preprocess_dataset(dataset):
+def preprocess_dataset(dataset, model_checkpoint):
 
-    tokenizer = AutoTokenizer.from_pretrained(MODEL_CHECKPOINT, use_fast=True)
+    tokenizer = AutoTokenizer.from_pretrained(model_checkpoint, use_fast=True)
 
     def preprocess_function(examples):
         output = tokenizer(examples["comment_text"], padding="max_length", truncation=True)
@@ -59,9 +54,9 @@ def preprocess_dataset(dataset):
         factclaiming = examples["Sub3_FactClaiming"]
         labels = []
         for t, e, f in zip(toxic, engaging, factclaiming):
-            t = 1. if t == 1 else -1.
-            e = 1. if e == 1 else -1.
-            f = 1. if f == 1 else -1.
+            t = 1.0 if t == 1 else -1.0
+            e = 1.0 if e == 1 else -1.0
+            f = 1.0 if f == 1 else -1.0
             labels.append([t, e, f])
         output["labels"] = labels
         return output
--- train.py
+++ train.py
@@ -1,55 +1,77 @@
+import random
+
 import numpy as np
 import torch
+import typer
 from datasets import load_metric
+from loguru import logger
 from transformers import (AutoModelForSequenceClassification, AutoTokenizer,
                           Trainer, TrainingArguments)
 
-from parameters import (BATCH_SIZE, LEARNING_RATE, MODEL_CHECKPOINT, NB_EPOCH,
-                        NUM_LABELS, OUTPUT_DIR)
-from dataset import load_train, load_test
-from loguru import logger
+from dataset import load_test, load_train
+
+app = typer.Typer()
 
 
-logger.info(f"Load the model: {MODEL_CHECKPOINT}.")
-model = AutoModelForSequenceClassification.from_pretrained(MODEL_CHECKPOINT, num_labels=NUM_LABELS)
+@app.command()
+def train(
+    seed: int = 42,
+    train_csv: str = "data/train.csv",
+    train_train_csv: str = "data/train.train.csv",
+    train_test_csv: str = "data/train.test.csv",
+    train_ratio: int = 0.8,
+    num_labels: int = 3,
+    model_checkpoint: str = "deepset/gbert-base",
+    output_dir: str = "models/",
+    batch_size: int = 16,
+    learning_rate: float = 2e-5,
+    nb_epoch: int = 5,
+):
+    random.seed(seed)
+    output_dir += model_checkpoint
+    logger.info(f"Load the model: {model_checkpoint}.")
+    model = AutoModelForSequenceClassification.from_pretrained(model_checkpoint, num_labels=num_labels)
 
-args = TrainingArguments(
-    output_dir=OUTPUT_DIR,
-    evaluation_strategy="epoch",
-    learning_rate=LEARNING_RATE,
-    per_device_train_batch_size=BATCH_SIZE,
-    per_device_eval_batch_size=BATCH_SIZE,
-    num_train_epochs=NB_EPOCH,
-    weight_decay=0.01,
-    load_best_model_at_end=True,
-    metric_for_best_model="accuracy",
-    logging_dir='./logs',
-    logging_steps=10,
-)
+    args = TrainingArguments(
+        output_dir=output_dir,
+        evaluation_strategy="epoch",
+        learning_rate=learning_rate,
+        per_device_train_batch_size=batch_size,
+        per_device_eval_batch_size=batch_size,
+        num_train_epochs=nb_epoch,
+        weight_decay=0.01,
+        load_best_model_at_end=True,
+        metric_for_best_model="accuracy",
+        logging_dir="./logs",
+        logging_steps=10,
+    )
 
-metric = load_metric("metrics/singleclass.py")
+    metric = load_metric("metrics/singleclass.py")
 
-def compute_metrics(eval_pred):
-    predictions, labels = eval_pred
-    predictions = np.sign(predictions)
-    return metric.compute(predictions=predictions.astype("int32"), references=labels.astype("int32"))
+    def compute_metrics(eval_pred):
+        predictions, labels = eval_pred
+        predictions = np.sign(predictions)
+        return metric.compute(predictions=predictions.astype("int32"), references=labels.astype("int32"))
 
-logger.info("Load and preprocess the dataset.")
-train_dataset = load_train(preprocess=True)
-test_dataset = load_test(preprocess=True)
-tokenizer = AutoTokenizer.from_pretrained(MODEL_CHECKPOINT, use_fast=True)
+    logger.info("Load and preprocess the dataset.")
+    train_dataset = load_train(train_train_csv, model_checkpoint, preprocess=True)
+    test_dataset = load_test(train_test_csv, model_checkpoint, preprocess=True)
+    tokenizer = AutoTokenizer.from_pretrained(model_checkpoint, use_fast=True)
 
-trainer = Trainer(
-    model,
-    args,
-    train_dataset=train_dataset,
-    eval_dataset=test_dataset,
-    tokenizer=tokenizer,
-    compute_metrics=compute_metrics
-)
+    trainer = Trainer(
+        model,
+        args,
+        train_dataset=train_dataset,
+        eval_dataset=test_dataset,
+        tokenizer=tokenizer,
+        compute_metrics=compute_metrics,
+    )
 
-logger.info("Start the training.")
-trainer.train()
+    logger.info("Start the training.")
+    trainer.train()
 
-logger.info("Start the evaluation.")
-trainer.evaluate()
+    logger.info("Start the evaluation.")
+    trainer.evaluate()
+
+if __name__ == "__main__":
+    app()
