df602cdd3b4d1b8914470ff592a81d19f771d0ec
airKlizz <remi.calizzano@gmail.com>
2021-05-31 17:46:49 +0200

    debug 3

--- train.py
+++ train.py
@@ -324,9 +324,6 @@ def hyperparameter_search_singleclass(
 
         def compute_metrics(eval_pred):
             logits, labels = eval_pred
-            print("LABELS")
-            print(labels)
-            print(type(labels))
             labels = [0 if label == 375 else 1 for label in labels]
             logits = torch.tensor(logits[0]).squeeze(1)
             selected_logits = logits[:, [375, 36339]]  # no=375 yes=36339
