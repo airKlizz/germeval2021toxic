746bc9102b5cae12e452153d86e0bfd7f10fbea9
airKlizz <remi.calizzano@gmail.com>
2021-06-04 12:25:27 +0200

    create submission

--- predict.py
+++ predict.py
@@ -2,6 +2,7 @@ import random
 from typing import List
 
 import numpy as np
+import pandas as pd
 import torch
 import torch.nn.functional as F
 import typer
@@ -94,6 +95,65 @@ def predict(
     stats = metric.compute(predictions=all_predictions, references=all_labels)
     print(stats)
     return stats
+
+
+@app.command()
+def create_submission(
+    test_csv: str = "data/toxic_kaggle/test.csv",
+    model_checkpoint: str = "deepset/gbert-base",
+    model_type: str = "auto",
+    batch_size: int = 16,
+    max_length: int = 256,
+    output_file: str = "submission.csv",
+):
+    logger.info(f"Start singleclass prediction.")
+    logger.info(f"Load the model: {model_checkpoint}.")
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    if model_type == "auto":
+        model = AutoModelForSequenceClassification.from_pretrained(model_checkpoint, num_labels=2).to(device)
+    elif model_type == "t5":
+        model = MT5ForConditionalGeneration.from_pretrained(model_checkpoint).to(device)
+    else:
+        raise NotImplementedError("Model type available: 'auto' or 't5'")
+
+    if model_type == "auto":
+
+        def get_predictions(outputs):
+            return np.argmax(outputs.logits.tolist(), axis=1).tolist()
+
+    elif model_type == "t5":
+
+        def get_predictions(outputs):
+            logits = outputs.logits.squeeze(1)
+            selected_logits = logits[:, [59006, 112560]]
+            probs = F.softmax(selected_logits, dim=1)
+            return np.argmax(probs.tolist(), axis=1).tolist()
+
+    else:
+        raise NotImplementedError("Model type available: 'auto' or 't5'")
+
+    logger.info("Load and preprocess the dataset.")
+    logger.debug(f"test_csv: {test_csv}")
+    dataset = load(test_csv, model_checkpoint, model_type, preprocess=True, labels=[], max_length=max_length)
+    if model_type == "auto":
+        columns = ["input_ids", "token_type_ids", "attention_mask"]
+    elif model_type == "t5":
+        columns = ["input_ids", "attention_mask", "decoder_input_ids"]
+    else:
+        raise NotImplementedError("Model type available: 'auto' or 't5'")
+    dataset.set_format(type="torch", columns=columns)
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size)
+
+    all_predictions = []
+    for batch in tqdm(dataloader, desc="In progress..."):
+        batch = {k: v.to(device) for k, v in batch.items()}
+        outputs = model(**batch)
+        predictions = get_predictions(outputs)
+        all_predictions += predictions
+
+    ids = dataset["id"]
+    df = pd.DataFrame(columns=["id", "prediction"], data=zip(*[ids, all_predictions]))
+    df.to_csv(output_file)
 
 
 if __name__ == "__main__":
