6abb3b19ec12e5d722254c89f9b850e8f5a1feb7
airklizz <remi.calizzano@gmail.com>
2021-06-30 16:53:18 +0200

    new f

--- predict.py
+++ predict.py
@@ -135,6 +135,95 @@ def predict(
     print(stats)
     return stats
 
+@app.command()
+def joint_predict(
+    test_csv: str = "data/train.test.csv",
+    model_checkpoints: List[str] = ["deepset/gbert-base"],
+    model_types: List[str] = ["auto"],
+    batch_size: int = 16,
+    max_length: int = 256,
+    binary: bool = True,
+    output_file: str = "joint_prediction.csv",
+    to_file: bool = False,
+):
+    logger.info(f"Start singleclass joint prediction.")
+    metric = load_metric("metrics/singleclass.py")
+
+    all_models_predictions = []
+    for model_checkpoint, model_type in zip(model_checkpoints, model_types):
+        logger.info(f"Load the model: {model_checkpoint}.")
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        if model_type == "auto":
+            model = AutoModelForSequenceClassification.from_pretrained(model_checkpoint, num_labels=2).to(device)
+        elif model_type == "t5":
+            model = MT5ForConditionalGeneration.from_pretrained(model_checkpoint).to(device)
+        else:
+            raise NotImplementedError("Model type available: 'auto' or 't5'")
+
+        if model_type == "auto":
+
+            def get_predictions(outputs):
+                if binary:
+                    return np.argmax(outputs.logits.tolist(), axis=1).tolist()
+                return outputs.logits.tolist()[1]
+
+        elif model_type == "t5":
+
+            def get_predictions(outputs):
+                logits = outputs.logits.squeeze(1)
+                selected_logits = logits[:, [59006, 112560]]
+                probs = F.softmax(selected_logits, dim=1)
+                if binary:
+                    return np.argmax(probs.tolist(), axis=1).tolist()
+                return probs.tolist()[1]
+
+        else:
+            raise NotImplementedError("Model type available: 'auto' or 't5'")
+
+        logger.info("Load and preprocess the dataset.")
+        logger.debug(f"test_csv: {test_csv}")
+        dataset = load(test_csv, model_checkpoint, model_type, preprocess=True, labels=[], max_length=max_length)
+        if model_type == "auto":
+            columns = ["input_ids", "token_type_ids", "attention_mask"]
+        elif model_type == "t5":
+            columns = ["input_ids", "attention_mask", "decoder_input_ids"]
+        else:
+            raise NotImplementedError("Model type available: 'auto' or 't5'")
+        final_columns = []
+        for column in columns:
+            if column in dataset.column_names:
+                final_columns.append(column)
+        columns = final_columns
+
+        dataset.set_format(type="torch", columns=columns)
+        dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size)
+
+        all_predictions = []
+        for batch in tqdm(dataloader, desc="In progress..."):
+            batch = {k: v.to(device) for k, v in batch.items()}
+            outputs = model(**batch)
+            predictions = get_predictions(outputs)
+            all_predictions += predictions
+
+        all_models_predictions.append(all_predictions)
+
+    final_predictions = []
+    for models_prediction in zip(*all_models_predictions):
+        assert len(models_prediction) == len(model_checkpoints)
+        final_predictions.append(round(sum(models_prediction) / len(models_prediction)))
+
+    try: 
+        ids = dataset["id"]
+    except:
+        try:
+            ids = dataset["comment_id"]
+        except:
+            ids = dataset["comment_text"]
+    df = pd.DataFrame(columns=["id"] + model_checkpoints, data=zip(*([ids] + all_models_predictions)))
+    if to_file:
+        df.to_csv(output_file)
+
+
 
 @app.command()
 def create_submission(
@@ -215,6 +304,7 @@ def create_submission(
         predictions1  = list(list(zip(*all_predictions))[1])
         df = pd.DataFrame(columns=["id", "prediction0", "prediction1"], data=zip(*[ids, predictions0, predictions1]))
     df.to_csv(output_file)
+
 
 
 if __name__ == "__main__":
